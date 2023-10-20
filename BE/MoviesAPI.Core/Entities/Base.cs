24bab41c3ebfb18ea63c272a87c2bcdb5638be58
Daniel99W <daniel.costache11@gmail.com>
2023-10-20 17:43:17 +0300

    Add basic configuration - DAL , Core, Entities

---
+++ BE/MoviesAPI.Core/Entities/Base.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.Core.Entities
+{
+    public class Base
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ BE/MoviesAPI.Core/Entities/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.Core.Entities
+{
+    public class Movie : Base
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Views { get; set; }
+        public int Upvote { get; set; }
+        public int Downvote { get; set; }
+        public DateTime AddedDate { get; set; }
+    }
+}
